c25a8185e978b9b2dac4c5947856cd8d76ae0b75
*anintiqa* <*abhi199131@gmail.com*>
2024-05-06 14:57:57 +0530

    Theory 2

---
+++ For loop/Nested loop.py
@@ -0,0 +1,8 @@
+## Nested loop will work inside the loop
+##as we can take i in outer loop with internal j loop
+#where i will excute the loop inside i  and follow with j all values
+
+for i in range(0,5):
+    for j in range(0,5):
+        print('i,j)
+
